24496b031be9d42cabc8000fe4bf355fb95292af
Mukund <2255mukund@gmail.com>
2022-08-02 18:55:53 +0530

    Nothing bro chumma

--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
 minikube start
-helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 helm install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets
 helm upgrade --install loki grafana/loki-stack --set grafana.enabled=true,prometheus.enabled=true -n loki --create-namespace
-kubectl apply -f ./mongo_config.yaml
-kubeseal --controller-name=sealed-secrets --controller-namespace=kube-system --format yaml --cert mycert.pem < ./mongo-secret.yaml > ./mongo-sealedsecret.yaml
+kubeseal --controller-name=sealed-secrets-controller --controller-namespace=kube-system --format yaml --cert mycert.pem < ./mongo-secret.yaml > ./mongo-sealedsecret.yaml
 kubectl apply -f ./mongo_config.yaml
 kubectl apply -f ./mongo-sealedsecret.yaml
 kubectl apply -f ./flaskapp_deployment.yaml
@@ -15,10 +13,9 @@ kubectl apply -f ./mongo.yaml
 kubectl apply -f ./mongo-svc.yaml
 kubectl apply -f ./mongo-pv.yaml
 kubectl apply -f ./mongo-pvc.yaml
-kubectl rollout status deployment/flaskapp
-kubectl rollout status deployment/mongo
+sleep 60 && echo "Waiting for Deployment!"
 kubectl get svc
 kubectl get pv
 kubectl get pvc
-minikube service flaskapp-svc --url
+curl $( minikube service flaskapp-svc --url )
 kubectl port-forward --namespace loki service/loki-grafana 3000:80
